b48dea5b7d9bf60b18535db92e67f30412149ccb
Igor Sinitsyn <igor.sinitsyn@westwing.ru>
2019-05-03 12:11:04 +0300

    Initial commit

---
+++ src/ZooShopDomain/src/ValueObjects/Brand/BrandType.php
@@ -0,0 +1,40 @@
+<?php
+namespace ZooShopDomain\ValueObjects\Brand;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use StockDomain\ValueObjects\Quantity\QuantityVO;
+
+/**
+ * Quantity data type
+ */
+class BrandType extends Type
+{
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'VARCHAR(100)';
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        try {
+            return new BrandVO($value);
+        } catch (\Exception $exception) {
+        }
+    }
+
+    /**
+     * @param QuantityVO $value
+     * @param AbstractPlatform $platform
+     * @return mixed
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->get();
+    }
+
+    public function getName()
+    {
+        return BrandVO::NAME;
+    }
+}
