f57a6d7ef179fa645765660dc15f67f97435f767
Igor Sinitsyn <igor.sinitsyn@westwing.ru>
2019-05-05 18:42:00 +0300

    Adds translates

---
+++ data/translates/ru_RU.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'CREATE_PRODUCT_FORM_TITLE_LABEL' => 'Название',
+    'CREATE_PRODUCT_FORM_TITLE_PLACEHOLDER' => 'Введите название',
+    'CREATE_PRODUCT_FORM_CATEGORY_LABEL' => 'Категория',
+    'CREATE_PRODUCT_FORM_CATEGORY_DEFAULT' => 'Выберите категорию',
+    'DOG_COSMETICS_CATEGORY' => 'Косметика для собак',
+    'SAVE_BUTTON' => 'Сохранить',
+    'PRODUCT_TITLE_IS_EMPTY' => 'Название продукта должно быть заполнено',
+    'CATEGORY_IS_EMPTY' => 'Категория не выбрана'
+];
