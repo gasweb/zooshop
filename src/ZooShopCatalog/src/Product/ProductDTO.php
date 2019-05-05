cff99c4c5fe952b236a2191930ce60699de911be
Igor Sinitsyn <igor.sinitsyn@westwing.ru>
2019-05-05 18:45:14 +0300

    Initial commit

---
+++ src/ZooShopCatalog/src/Product/ProductDTO.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace ZooShopCatalog\Product;
+
+class ProductDTO
+{
+    private $title;
+
+    private $category;
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param mixed $category
+     */
+    public function setCategory($category): void
+    {
+        $this->category = $category;
+    }
+}
